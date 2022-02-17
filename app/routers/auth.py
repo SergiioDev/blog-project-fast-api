936a4145132ecaa85a90d5b4868f26f6dee125ee
SergiioDev <sergi13456@hotmail.es>
2022-02-17 11:31:37 +0100

    return the token schema in the login endpoint

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -2,16 +2,14 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from ..database import get_db
 from sqlalchemy.orm import Session
-from .. import models
-from .. import utils
-from .. import oauth2
+from .. import models, utils, oauth2, schemas
 
 router = APIRouter(
     tags=['Authentication']
 )
 
 
-@router.post("/login")
+@router.post("/login", response_model=schemas.Token)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 

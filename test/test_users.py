d41166f5c499e20d048c37cbd29a041819cf535d
SergiioDev <sergi13456@hotmail.es>
2022-03-05 23:59:47 +0100

    create test database

---
+++ test/test_users.py
@@ -0,0 +1,39 @@
+from fastapi.testclient import TestClient
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.main import app
+from app.config import settings
+from app.database import get_db
+from app.database import Base
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}_test'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+@pytest.fixture
+def client():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    yield TestClient(app)
+
+
+def test_create_user(client):
+    response = client.post("/users/",
+                           json={"email": "unitTest@unit.com", "password": "unit"})
+    assert response.status_code == 201
+    assert response.json() == {"id": 1, "email": "unitTest@unit.com"}

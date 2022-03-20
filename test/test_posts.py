4949cb910b3a7f0e4b893c1e4b26e177ad21bc1d
SergiioDev <sergi13456@hotmail.es>
2022-03-20 12:18:29 +0100

    finish post unit test

--- test/test_posts.py
+++ test/test_posts.py
@@ -1,3 +1,18 @@
 def test_get_all_posts(authorized_client):
     response = authorized_client.get("/posts/")
     assert response.status_code == 200
+
+
+def test_get_one_post(authorized_client, test_post):
+    res = authorized_client.get(f"/posts/{test_post[0].id}")
+    assert res.status_code == 200
+
+
+def test_unauthorized_user_get_one_post(client, test_post):
+    res = client.get(f"/posts/{test_post[0].id}")
+    assert res.status_code == 401
+
+
+def test_get_one_post_not_exist(authorized_client, test_post):
+    res = authorized_client.get(f"/posts/22")
+    assert res.status_code == 404

a0bf2bd8fc79aa3b106a0ea56681b2907204199a
SergiioDev <sergi13456@hotmail.es>
2022-03-20 12:23:50 +0100

    finish post unit test

--- test/test_posts.py
+++ test/test_posts.py
@@ -1,18 +1,27 @@
+from app.schemas import PostResponse
+
+
 def test_get_all_posts(authorized_client):
     response = authorized_client.get("/posts/")
     assert response.status_code == 200
 
 
 def test_get_one_post(authorized_client, test_post):
-    res = authorized_client.get(f"/posts/{test_post[0].id}")
-    assert res.status_code == 200
+    response = authorized_client.get(f"/posts/{test_post[0].id}")
+    post = PostResponse(**response.json())
+    assert post.Post.id == test_post[0].id
+    assert post.Post.title == test_post[0].title
+    assert post.Post.content == test_post[0].content
+    assert post.Post.published == test_post[0].published
 
 
 def test_unauthorized_user_get_one_post(client, test_post):
-    res = client.get(f"/posts/{test_post[0].id}")
-    assert res.status_code == 401
+    response = client.get(f"/posts/{test_post[0].id}")
+    assert response.status_code == 401
 
 
 def test_get_one_post_not_exist(authorized_client, test_post):
-    res = authorized_client.get(f"/posts/22")
-    assert res.status_code == 404
+    response = authorized_client.get(f"/posts/22")
+    assert response.status_code == 404
+
+
